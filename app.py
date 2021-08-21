fe96420959aa8b7d8b9e80deb5ee3506467f17b2
David <david.anthony.lei@gmail.com>
2021-08-22 07:55:39 +1000

    txt bot

--- app.py
+++ app.py
@@ -7,15 +7,35 @@ app = Flask(__name__)
 def hello():
 	return "hello world <3"
 
+counter = 0
+
+@app.route('/reset')
+def reset():
+	counter = 0
+	return "counter is: " + str(counter)
+
+@app.route('/counter')
+def counter():
+	return "counter is: " + str(counter)
+
+RESP = {
+	0: "hello izzie",
+	1: "or should i call you?...",
+	2: "isadora",
+	3: "good morning",
+	4: "wake me up inside"
+}
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
     resp = MessagingResponse()
-
+    if counter in RESP:
+    	msg = RESP[counter]
+    else:
+    	msg = "good luck buddy!"
     # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
-
+    resp.message(msg)
     return str(resp)
 
 if __name__ == "__main__":
